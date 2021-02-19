e240504506aae7f388a5a502ee56982b802ee1a3
Zihao <zihaozhang1@gmail.com>
2021-02-19 00:49:40 -0500

    npm init + connection code for sql/jawsdb

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+// Create Connection
+const db_config = {
+  host: '',
+  user: '',
+  password: '',
+  database: '',
+  port: ''
+};
+
+module.exports = db_config;
