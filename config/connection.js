7fd2718af3b5d8973f44a8b56b8e1500952f16fa
Zihao <zihaozhang1@gmail.com>
2021-02-19 01:23:59 -0500

    established jawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 // Create Connection
 const db_config = {
-  host: '',
-  user: '',
-  password: '',
-  database: '',
-  port: ''
+  host: 'd6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'qmvpb7uhcujx5o1s',
+  password: 's0s2y9vfvz3r3izl',
+  database: 'nnzc5zz5340alt2m',
+  port: '3306'
 };
 
 module.exports = db_config;
